0d87c07e38419c9688208ec341caf1cabf63e2c7
Alexander <lltwox@gmail.com>
2013-05-25 14:01:39 +0400

    Static prototype

--- public/js/actions.js
+++ public/js/actions.js
@@ -1,15 +1,6 @@
 $(function() {
     $('#ask').click(function() {
         $('.question-wrapper').css('margin-top', '3%');
-        $('#ask').hide();
-        $('#retry').show();
         $('.answer').fadeIn(300);
-    });
-
-    $('#retry').click(function() {
-        $('#ask').show();
-        $('#retry').hide();
-        $('.answer').hide();
-        $('.question-wrapper').css('margin-top', '18%');
     });
 });
--- public/js/requests.js
+++ public/js/requests.js
@@ -1,10 +1,68 @@
 $(function() {
-    var BACKEND = 'http://pogodable.ru/api/';
+    var BACKEND = 'http://pogodable.ru/api';
 
-    $.get(
-        BACKEND + 'activitys',
-        function(response) {
-            console.log(response);
+    $.get(BACKEND + '/activitys', function(list) {
+        for (var index in list) {
+            var element = $('<option value="' +
+                list[index]['_id'] + '">' +
+                list[index]['activity'] +
+                '</option>');
+
+            $('.question .activity').append(element);
         }
-    );
+    });
+
+    (function() {
+        // generating dates
+        var captions = {
+            0: 'в воскресенье',
+            1: 'в понедельник',
+            2: 'во вторник',
+            3: 'в среду',
+            4: 'в четверг',
+            5: 'в пятницу',
+            6: 'в субботу'
+        };
+
+        var currentTime = new Date().getTime();
+        for (var i = 0; i <= 6; i++) {
+            var caption = null,
+                dateObj = new Date(
+                    currentTime + i * 60 * 60 * 24 * 1000),
+                dateStr = dateObj.toString('yyyy-MM-dd'),
+                element = null;
+
+            if (i === 0) {
+                caption = 'сегодня';
+            } else if (i === 1) {
+                caption = 'завтра';
+            } else {
+                caption = captions[dateObj.getDay()];
+            }
+
+            element = $('<option value="' +
+                dateStr + '">' + caption +
+                '</option>');
+
+            $('.question .day').append(element);
+        }
+
+    })();
+
+    $('#ask').click(function() {
+        $.get(
+            BACKEND + '/suggestions/find',
+            {
+                'activity': $('.question .activity').val(),
+                'date': $('.question .day').val(),
+                'timeOfDay': $('.question .timeOfDay').val()
+            },
+            function(response) {
+                if (response && response['suggestion']) {
+                    var element = $(response['suggestion']).wrap('<p>');
+                    $('.answer').html(element);
+                }
+            }
+        );
+    });
 });
