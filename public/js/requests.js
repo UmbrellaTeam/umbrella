cb1cd6383b0bca3449281f81fd004c2d59419f23
Alexander <lltwox@gmail.com>
2013-05-26 13:46:48 +0400

    totally new style

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,78 +1,64 @@
 $(function() {
 	var BACKEND = 'http://pogodable.ru/api';
 
-	$.get(BACKEND + '/activitys/', function(list) {
-		for (var index in list) {
-			var element = $('<option value="' +
-				list[index]['_id'] + '">' +
-				list[index]['activity'] +
-				'</option>');
+    var loadAnswer = function() {
+        $('.answer-wrapper').hide();
+        $.post(
+            BACKEND + '/suggestions/find',
+            {
+                'activity': $('.question .activity li.active').attr('data-id'),
+                'date': $('.question .date li.active').attr('data-id'),
+                'timeOfDay': 'morning'
+            },
+            function(response) {
+                if(response && response.suggestion) {
+                    $('.answer-wrapper').fadeIn(300);
+                    $('.answer div.suggestion').html(response.suggestion.suggestion);
+                    if(response.weather) {
+                        $('.answer .weather-list').empty();
+                        for (var i in response.weather) {
+                            if (i == 'night') {
+                                continue;
+                            }
+                            var currentWeather = response.weather[i];
+                            console.log(currentWeather);
+                            $('.answer .weather-list').append($(
+                                '<div class="weather">' +
+                                    '<img src="/img/' + currentWeather.cloudiness + '.png"></img>' +
+                                    '<span class="degrees">+' +
+                                        currentWeather.temperatureMin +
+                                        '…+' +
+                                        currentWeather.temperatureMax +
+                                    '</span>' +
+                                '</div>'
+                            ));
+                        }
+                    }
+                }
+            }
+        );
+    };
 
-			$('.question .activity').append(element);
-		}
-		setDefaultSelect();
-	});
+	$('#ask').click(loadAnswer);
 
-	(function() {
-		// generating dates
-		var captions = {
-			0: 'в воскресенье',
-			1: 'в понедельник',
-			2: 'во вторник',
-			3: 'в среду',
-			4: 'в четверг',
-			5: 'в пятницу',
-			6: 'в субботу'
-		};
+    var selectElement = function(id) {
+        var element = $('.question li[data-id="' + id + '"]');
+        if (element.hasClass('active')) {
+            return;
+        }
 
-		var currentTime = new Date().getTime();
-		for (var i = 0; i <= 6; i++) {
-			var caption = null,
-				dateObj = new Date(
-					currentTime + i * 60 * 60 * 24 * 1000),
-				dateStr = dateObj.toString('yyyy-MM-dd'),
-				element = null;
+        var otherElement = element.prev();
+        otherElement.insertAfter(element).removeClass('active');
+        element.addClass('active');
+    };
 
-			if(i === 0) {
-				caption = 'сегодня';
-			} else if(i === 1) {
-				caption = 'завтра';
-			} else {
-				caption = captions[dateObj.getDay()];
-			}
+    $('.question li a').on('click', function() {
+        var $this = $(this);
+        var id = $this.parent().attr('data-id');
+        selectElement(id);
 
-			element = $('<option value="' +
-				dateStr + '">' + caption +
-				'</option>');
-
-			$('.question .day').append(element);
-		}
-
-	})();
-
-	$('#ask').click(function() {
-		$('.answer-wrapper').hide();
-		$.post(
-			BACKEND + '/suggestions/find',
-			{
-				'activity': $('.question .activity').val(),
-				'date': $('.question .day').val(),
-				'timeOfDay': $('.question .timeOfDay').val()
-			},
-			function(response) {
-				if(response && response.suggestion) {
-					$('.question').addClass('topped');
-					$('.answer-wrapper').fadeIn(300);
-					$('.answer div.suggestion').html(response.suggestion.suggestion);
-					if(response.weather) {
-						$('.answer img').attr('src', '/img/' + response.weather.cloudiness + '.png');
-                        $('.answer .degrees').text('+' + response.weather.temperatureMin + '…+' + response.weather.temperatureMax);
-					}
-                    console.log(response);
-				}
-			}
-		);
-	});
+        return false;
+    });
 
 	var setDefaultSelect = function() {
 		var time = new Date();
@@ -80,13 +66,12 @@ $(function() {
 			hours = time.getHours(),
 			activity = "51a08e807aebff8865000003",
 			timeOfDay = "evening";
-		/* activity */
-		if(day > 0 && day < 6) activity = "51a08e527aebff8865000002";
-		$("select.activity").val(activity);
-		/* timeOfDay */
-		if(hours > 5 && hours < 11) timeOfDay = "morning";
-		if(hours >= 11 && hours < 18) timeOfDay = "day";
-        if(hours >= 0 && hours <= 5) timeOfDay = "night";
-        $("select.timeOfDay").val(timeOfDay);
+
+		if(day > 0 && day < 6) {
+            activity = "51a08e527aebff8865000002";
+        }
+		selectElement(activity);
 	};
+
+    loadAnswer();
 });
